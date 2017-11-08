0c7e539ad47599204172d66ea323bec6d830ef0a
CJuice <CJuice@users.noreply.github.com>
2017-11-08 11:35:25 -0500

    Revisions
    Reached a full successful run of process on multiple images in a consolidated location.

--- TaxMapProcessing_V1a.py
+++ TaxMapProcessing_V1a.py
@@ -1,10 +1,11 @@
 ####################
+# Author: CJS
+# Date: 20171108
 #
 #
 #
-#
-#
-#
+# ASSUMPTIONS:  All files have been run through the Step 1 process.
+#               All TIF images have a TFW.
 ####################
 #TODO: once script stabilizes refine imports to slim imported content
 # IMPORTS
@@ -13,6 +14,7 @@ from sys import exit
 from arcpy import management
 from arcpy import env
 from arcpy import SpatialReference
+from arcpy import GetMessages
 from datetime import date
 import ImageClass
 
@@ -24,9 +26,10 @@ lsTifFilesInImagesFolder = None
 intDefineProjectionCode = 26985 # WKID 2248 is feet, WKID 26985 is meters
 intProjectRasterCode = 3857 # WKID 3857 is web mercator
 objSpatialReferenceProjectedRaster = SpatialReference(intProjectRasterCode)
-strRasterDatasetName = "RD_{}".format(strDateToday)
+# strRasterDatasetName = "RD_{}".format(strDateToday)
 strRasterCatalogName = "RCmanaged_{}".format(strDateToday)
 lsImageObjects = []
+lsUnsuccessfulImageReProjections = []
 
 # INPUTS
     # Get the path for the consolidated images files folder
@@ -50,23 +53,39 @@ except:
     print "Error in checking workspace path existence. Exiting."
     exit()
 
-    # Create Raster Dataset in workspace
-print "Creating Raster Dataset in Workspace"
-try:
-    management.CreateRasterDataset(out_path=env.workspace,
-                                   out_name=strRasterDatasetName,
-                                   cellsize=None,
-                                   pixel_type="1_BIT",
-                                   raster_spatial_reference=intProjectRasterCode,
-                                   number_of_bands=1,
-                                   config_keyword=None,
-                                   pyramids=None,
-                                   tile_size=None,
-                                   compression=None,
-                                   pyramid_origin=None)
-except:
-    print "Error creating raster dataset. Exiting."
+print "Defining projection..."
 
+try:
+    for (dirname, dirs, files) in os.walk(strConsolidatedImageFileFolderPath):
+        for file in files:
+            if (str(file)).endswith("tif"):
+                # Build image object, set properties, and store in list
+                objImage = ImageClass.Image(dirname, str(file))
+                objImage.setFileName_lower()
+                print "Defining projection... {}".format(objImage.getFileName_lower())
+                management.DefineProjection(in_dataset=objImage.getFilePath_Original(),
+                                            coor_system=intDefineProjectionCode)
+                print "Define Projection successful"
+                print "Re-Projecting... {}".format(objImage.getFileName_lower())
+                try:
+                    management.ProjectRaster(in_raster=objImage.getFilePath_Original(),
+                                             out_raster=objImage.getFileName_lower(),
+                                             out_coor_system=objSpatialReferenceProjectedRaster,
+                                             resampling_type=None,
+                                             cell_size=None,
+                                             geographic_transform="NAD_1983_To_WGS_1984_1",
+                                             Registration_Point=None,
+                                             in_coor_system=None)
+                    print "Re-Project successful."
+                except:
+                    print "Project unsuccessful for {}. Skipping file.".format(objImage.getFileName_lower())
+                    lsUnsuccessfulImageReProjections.append(objImage.getFileName_lower())
+            else:
+                continue
+except:
+    print "Error walking directory and creating Image object."
+    exit()
+try:
     # Create Raster Catalog in workspace
     management.CreateRasterCatalog(out_path=env.workspace,
                                    out_name=strRasterCatalogName,
@@ -78,77 +97,27 @@ except:
                                    spatial_grid_3=0,
                                    raster_management_type="MANAGED",
                                    template_raster_catalog=None)
-
-    # Define projection and reproject the rasters
-
-env.workspace = os.path.join(env.workspace,strRasterDatasetName)
-print "Switching workspace to {}.".format(env.workspace)
-print "Define projection and re-projection underway..."
-
-#TODO: Run this again, determine why a Raster Dataset did not show in the gdb and also why it errored out and exited below.
-#TODO: Why are the rasters I'm projecting showing as a file in the gdb instead of being inside the RD I precreated!?
-try:
-    for (dirname, dirs, files) in os.walk(strConsolidatedImageFileFolderPath):
-        print 1
-        for file in files:
-            print 2
-            if (str(file)).endswith("tif"):
-                print 3
-                # Build image object, set properties, and store in list
-                objImage = ImageClass.Image(dirname, str(file))
-                print 4
-                objImage.setFileName_lower()
-                print 5
-                management.DefineProjection(in_dataset=objImage.getFilePath_Original(),
-                                            coor_system=intDefineProjectionCode)
-                print "Define Projection successful: {}".format(objImage.getFileName_lower())
-                #TODO: If all tif.xml files prove to be the same then simply make a copy of the first xml file created by the Define Projection tool and rename new copy for each TIF
-                management.ProjectRaster(in_raster=objImage.getFilePath_Original(),
-                                         out_raster=objImage.getFileName_lower(),
-                                         out_coor_system=objSpatialReferenceProjectedRaster,
-                                         resampling_type=None,
-                                         cell_size=None,
-                                         geographic_transform="NAD_1983_To_WGS_1984_1",
-                                         Registration_Point=None,
-                                         in_coor_system=None)
-                print "Project successful: {}".format(objImage.getFileName_lower())
-            else:
-                continue
 except:
-    print "Error walking directory and creating Image object."
+    print "Error creating Raster Catalog"
+    print(GetMessages())
     exit()
-# try:
-#     for(dirname, dirs, files) in os.walk(strConsolidatedImageFileFolderPath):
-#         for file in files:
-#             strFileName = str(file)
-#             if strFileName.endswith("tif"):
-#                 print strFileName
-#                 strFileNameWithoutTifExtension = strFileName.rstrip(".tif")
-#                 strFilePath = os.path.join(dirname,strFileName)
-#                 management.DefineProjection(in_dataset=strFilePath,
-#                                             coor_system=intDefineProjectionCode)
-#                 print "Define Projection successful: {}".format(strFileName)
-#                 management.ProjectRaster(in_raster=strFilePath,
-#                                          out_raster=strFileNameWithoutTifExtension,
-#                                          out_coor_system=objSpatialReferenceProjectedRaster,
-#                                          resampling_type=None,
-#                                          cell_size=None,
-#                                          geographic_transform="NAD_1983_To_WGS_1984_1",
-#                                          Registration_Point=None,
-#                                          in_coor_system=None)
-#                 print "Project successful: {}".format(strFileName)
-#             else:
-#                 continue
-# except:
-#     print "Error in projection process. Exiting."
-#     exit()
-    # Load raster dataset into raster catalog
+
+print "Loading workspace into raster catalog..."
+    # Load raster datasets into raster catalog
 try:
-    print "Now loading raster dataset into raster catalog..."
     management.WorkspaceToRasterCatalog(env.workspace,
                                         strRasterCatalogName,
                                         include_subdirectories=None,
                                         project=None)
 except:
-    print "Error loading raster dataset into raster catalog"
+    print "Error loading workspace into {}".format(strRasterCatalogName)
+    print(GetMessages())
+    exit()
+
+print "Process complete."
+if len(lsUnsuccessfulImageReProjections) != 0:
+    print "The following list of lowercase filenames unsuccessfully Re-Projected\n{}".format(lsUnsuccessfulImageReProjections)
+else:
+    pass
+
 # DELETIONS
